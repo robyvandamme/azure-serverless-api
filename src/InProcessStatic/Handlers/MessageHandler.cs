6825429438b6af72e463b172261a1e459e8e21f3
Roby Van Damme <roby@robyvandamme.com>
2022-01-05 17:42:07 +0700

    Add In-Process HTTP function

---
+++ src/InProcessStatic/Handlers/MessageHandler.cs
@@ -0,0 +1,22 @@
+﻿using InProcessStatic.Models;
+
+namespace InProcessStatic.Handlers;
+
+public static class MessageHandler
+{
+    public static Message? Handle(NewMessage? newMessage)
+    {
+        if (newMessage == null)
+        {
+            return null;
+        }
+
+        if (string.IsNullOrWhiteSpace(newMessage.Content))
+        {
+            return null;
+        }
+
+        var message = new Message(newMessage.Content);
+        return message;
+    }
+}
